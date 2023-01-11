da2c3bc1d0b345b9b1175b236532812934386c8a
Kenneth Mensink <MENS411177@exactsoftware.com>
2023-01-11 14:15:44 +0100

    fixed layout

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { HeaderModule } from './header/header.module';
 import { SidebarModule } from './sidebar/sidebar.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [LayoutComponent],
-  imports: [CommonModule, HeaderModule, SidebarModule],
+  imports: [CommonModule, RouterModule, HeaderModule, SidebarModule],
   exports: [LayoutComponent],
 })
 export class LayoutModule {}
