68a3edf3f57878fb04728763440b587cc2d05c97
Kenneth Mensink <MENS411177@exactsoftware.com>
2023-01-11 15:48:12 +0100

    prototype sidebar done

--- src/app/layout/sidebar/menu.ts
+++ src/app/layout/sidebar/menu.ts
@@ -2,5 +2,52 @@ export interface MenuItem {
   url: string;
   icon?: string;
   text: string;
-  children: [MenuItem];
+  children?: MenuItem[];
 }
+
+export const MENU: MenuItem[] = [
+  {
+    url: '#',
+    icon: 'chat',
+    text: 'Forum',
+  },
+  {
+    url: '#',
+    icon: 'brush',
+    text: 'Blog',
+  },
+  {
+    url: '/',
+    icon: 'flask',
+    text: 'Research',
+  },
+  {
+    url: '#',
+    icon: 'inbox',
+    text: 'Inbox',
+  },
+  {
+    url: '#',
+    icon: 'user',
+    text: 'Users',
+    children: [
+      {
+        url: '#',
+        text: 'Overview',
+      },
+      {
+        url: '#',
+        text: 'Moderation',
+      },
+      {
+        url: '#',
+        text: 'Add user',
+      },
+    ],
+  },
+  {
+    url: '#',
+    icon: 'settings',
+    text: 'Settings',
+  },
+];
--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MENU, MenuItem } from './menu';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
-
+  menuItems: MenuItem[] = MENU;
 }
